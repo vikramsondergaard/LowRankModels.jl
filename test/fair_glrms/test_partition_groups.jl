559edff5eaa94060a3281663cc09b0c2ea535e3b
Vikram Sondergaard <u6390710@anu.edu.au>
2023-09-02 14:18:27 +1000

    completed ad observatory test for partition_groups()

--- test/fair_glrms/test_partition_groups.jl
+++ test/fair_glrms/test_partition_groups.jl
@@ -5,11 +5,6 @@ using DataFrames
 import XLSX
 import JSON
 
-ADULT_HEADERS = ["age", "workclass", "fnlwgt", "education", "education-num",
-                 "marital-status", "occupation", "relationship", "race", "sex",
-                 "capital-gain", "capital-loss", "hours-per-week",
-                 "native-country", "yearly-income"]
-
 """
     test(A, s::Int, expected)
 
@@ -30,8 +25,6 @@ function test(A, s::Int, expected)
     observed_categories = keys(expected)
     # First part of checking the keys are equivalent
     @assert length(observed_categories) == length(unique_categories)
-    display(unique_categories)
-    display(observed_categories)
     # Second part of checking the keys are equivalent
     for unique_cat in unique_categories
         @assert unique_cat in observed_categories
@@ -64,25 +57,44 @@ function test_trivial()
     println("Passed test_trivial()!")
 end
 
+"""
+    test_adult()
+
+Test that partition_groups() correctly partitions the Adult dataset based on
+gender.
+"""
 function test_adult()
     # Path to the Adult dataset
     datapath = "/Users/vikramsondergaard/honours/data/adult/adult.data"
-    # Read the CSV file
+    # Read the CSV file and convert it to a matrix
     adult_csv = CSV.read(datapath, DataFrame, header=false)
     A = convert(Matrix, adult_csv)
     # Path to a JSON file with the expected partition of the groups
     expected_path = "/Users/vikramsondergaard/honours/LowRankModels.jl/data/adult_partition_groups.json"
+    # Read the JSON file: this can stay as a Dict
     expected = JSON.parsefile(expected_path)
+    # Gender is the 10th column in the Adult data set
     index_of_gender = 10
     test(A, index_of_gender, expected)
     println("Passed test_adult()!")
 end
 
 function test_ad_observatory()
-end
-
-function test_german_credit()
+    # Path to the (condensed) Ad Observatory dataset
+    datapath = "/Users/vikramsondergaard/honours/LowRankModels.jl/data/ad_obs_observer_data.csv"
+    # Read in the Ad Observatory data and convert it to a matrix
+    ad_obs_data = CSV.read(datapath, DataFrame, header=1)
+    A = convert(Matrix, ad_obs_data)
+    # Path to a JSON file with the expected partition of the groups
+    expected_path = "/Users/vikramsondergaard/honours/LowRankModels.jl/data/ad_obs_partition_groups.json"
+    # Read the JSON file: this can stay as a Dict
+    expected = JSON.parsefile(expected_path)
+    # "Observer Income" is the 7th column in the Ad Observatory data set
+    index_of_income = 7
+    test(A, index_of_income, expected)
+    println("Passed test_ad_observatory()!")
 end
 
 test_trivial()
 test_adult()
+test_ad_observatory()
